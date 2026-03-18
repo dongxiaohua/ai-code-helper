94f698c6f22271fab527931d07cd7da4dd97a2ac
dongxiaohua <dong_healer@163.com>
2026-03-18 12:16:15 +0800

    langchain4j

--- src/main/java/com/example/aicodehelper/ai/AiCodeHelperServiceFactory.java
+++ src/main/java/com/example/aicodehelper/ai/AiCodeHelperServiceFactory.java
@@ -5,6 +5,7 @@ import dev.langchain4j.mcp.McpToolProvider;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.service.AiServices;
 import jakarta.annotation.Resource;
@@ -32,7 +33,7 @@ public class AiCodeHelperServiceFactory {
 
     // 引入流式输出
     @Resource
-    private StremingChatModel stremingChatModel;
+    private StreamingChatModel qwenStreamingChatModel;
 
     /**
      * 调用AIService.create创建AI service的实现类，背后原理李永乐Java反射机制创建了一个实现接口的代理对象，
@@ -48,7 +49,7 @@ public class AiCodeHelperServiceFactory {
 //        return AiServices.create(AiCodeHelperService.class, qwenChatModel);
         return AiServices.builder(AiCodeHelperService.class)
                 .chatModel(myQwenChatModel)
-                .streamingChatModel(stremingChatModel) // 流式输出
+                .streamingChatModel(qwenStreamingChatModel) // 流式输出
                 .chatMemory(chatMemory)
                 .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10)) // 会话记忆配置根据会话id实现会话隔离
                 .contentRetriever(contentRetriever) // RAG 检索增强生成
